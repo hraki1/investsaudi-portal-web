477898e07c2d65356dd8e93cbd348874539caff8
omazm <omer.saad@azm.dev>
2025-11-13 16:45:49 +0500

    WIP

--- src/components/sections/opportunities/Card.tsx
+++ src/components/sections/opportunities/Card.tsx
@@ -5,6 +5,7 @@ import ShareButton from './ShareButton';
 import { FiMail } from 'react-icons/fi';
 import { IoShareSocialOutline } from 'react-icons/io5';
 import { FaXTwitter } from 'react-icons/fa6';
+import { GoArrowUpRight } from 'react-icons/go';
 
 interface CardDetailsProps {
     title: string;
@@ -83,77 +84,96 @@ const Card: React.FC<CardProps> = ({
             </div>
 
 
-            <div className="flex-1 p-6 ">
-                <div className="space-y-4 text-white">
-                    <div className="space-y-2">
-                        <h3 className="text-2xl font-semibold ">{title}</h3>
-                        <div className="flex flex-row gap-4">
-                            <div className="text-sm tracking-wide flex flex-row gap-2 items-center"><MdOutlineFactory size="1.70em" color="var(--foundation-green)" />{category}</div>
-                            <div className="text-sm flex flex-row gap-2 items-center"><CiLocationOn size="1.70em" color="var(--foundation-green)" />{location}</div>
+            <div className="flex-1 relative">
+                <GradientBackground />
+                <div className="p-6">
+                    <div className="space-y-4 text-white">
+                        <div className="space-y-2">
+                            <h3 className="text-2xl font-semibold ">{title}</h3>
+                            <div className="flex flex-row gap-4">
+                                <div className="text-sm tracking-wide flex flex-row gap-2 items-center"><MdOutlineFactory size="1.70em" color="var(--foundation-green)" />{category}</div>
+                                <div className="text-sm flex flex-row gap-2 items-center"><CiLocationOn size="1.70em" color="var(--foundation-green)" />{location}</div>
+                            </div>
                         </div>
+
+                        <p className="text-base leading-relaxed">{description}</p>
+
+                        <div className="grid grid-cols-5 gap-4 items-start">
+                            <dl className='flex flex-col-reverse items-start gap-2'>
+                                <dt className="font-medium text-sm">Expected investment size</dt>
+                                <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{investment}</dd>
+                            </dl>
+                            <dl className='flex flex-col-reverse items-start gap-2'>
+                                <dt className="font-medium text-sm">Jobs Created</dt>
+                                <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{jobsCreated}</dd>
+                            </dl>
+                            <dl className='flex flex-col-reverse items-start gap-2'>
+                                <dt className="font-medium text-sm">GDP Impact</dt>
+                                <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{GDPImpact}</dd>
+                            </dl>
+                            <dl className='flex flex-col-reverse items-start gap-2'>
+                                <dt className="font-medium text-sm">Expected IRR</dt>
+                                <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{IRR}</dd>
+                            </dl>
+                            <dl className='flex flex-col-reverse items-start gap-2'>
+                                <dt className="font-medium text-sm">Payback Period</dt>
+                                <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{paybackPeriod}</dd>
+                            </dl>
+                        </div>
+
+                        <footer className="space-4 flex flex-row justify-between">
+
+                            <ul className="flex flex-row gap-4 pt-4 flex-wrap">
+                                <li>
+                                    <ShareButton
+                                    // onClick={handleShareEmail}
+                                    >
+                                        <FiMail size={16} />
+                                    </ShareButton>
+                                </li>
+                                <li>
+                                    <ShareButton
+                                    // onClick={handleSharePhone}
+                                    >
+                                        <MdOutlinePhoneInTalk size={16} />
+                                    </ShareButton>
+                                </li>
+                                <li>
+                                    <ShareButton
+                                    // onClick={handleShareSocial}
+                                    >
+                                        <IoShareSocialOutline size={16} />
+                                    </ShareButton>
+                                </li>
+                                <li>
+                                    <ShareButton
+                                    // onClick={handleShareTwitter}
+                                    >
+                                        <FaXTwitter size={16} />
+                                    </ShareButton>
+                                </li>
+
+                            </ul>
+                            <span className="flex items-center">
+
+                                <button className="text-base font-medium bg-teal-500 rounded-full px-3 py-2">I'm interested</button>
+                                <ShareButton className="ml-4">
+                                    <GoArrowUpRight size={16} />
+                                </ShareButton>
+                            </span>
+                        </footer>
                     </div>
-
-                    <p className="text-base leading-relaxed">{description}</p>
-
-                    <div className="grid grid-cols-5 gap-4 items-start">
-                        <dl className='flex flex-col-reverse items-start'>
-                            <dt className="font-medium text-sm">Expected investment size</dt>
-                            <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{investment}</dd>
-                        </dl>
-                        <dl className='flex flex-col-reverse items-start'>
-                            <dt className="font-medium text-sm">Jobs Created</dt>
-                            <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{jobsCreated}</dd>
-                        </dl>
-                        <dl className='flex flex-col-reverse items-start'>
-                            <dt className="font-medium text-sm">GDP Impact</dt>
-                            <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{GDPImpact}</dd>
-                        </dl>
-                        <dl className='flex flex-col-reverse items-start'>
-                            <dt className="font-medium text-sm">Expected IRR</dt>
-                            <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{IRR}</dd>
-                        </dl>
-                        <dl className='flex flex-col-reverse items-start'>
-                            <dt className="font-medium text-sm">Payback Period</dt>
-                            <dd className="font-semibold text-base whitespace-nowrap text-overflow-ellipsis overflow-hidden">~{paybackPeriod}</dd>
-                        </dl>
-                    </div>
-
-                    <ul className="space-4 flex flex-row gap-4 pt-4 flex-wrap">
-                        <li>
-                            <ShareButton
-                            // onClick={handleShareEmail}
-                            >
-                                <FiMail size={16} />
-                            </ShareButton>
-                        </li>
-                        <li>
-                            <ShareButton
-                            // onClick={handleSharePhone}
-                            >
-                                <MdOutlinePhoneInTalk size={16} />
-                            </ShareButton>
-                        </li>
-                        <li>
-                            <ShareButton
-                            // onClick={handleShareSocial}
-                            >
-                                <IoShareSocialOutline size={16} />
-                            </ShareButton>
-                        </li>
-                        <li>
-                            <ShareButton
-                            // onClick={handleShareTwitter}
-                            >
-                                <FaXTwitter size={16} />
-                            </ShareButton>
-                        </li>
-
-                    </ul>
                 </div>
             </div>
 
         </div>
     );
 };
+
+const GradientBackground: React.FC = () => {
+    return (
+        <div className="absolute w-full h-full z-50 bg-[radial-gradient(ellipse_at_bottom,var(--tw-gradient-stops))] from-emerald-500  to-transparent"></div>
+    );
+}
 
 export default Card;
